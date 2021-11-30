9c4ef1a62f505e3a61874e21e0a0f86f10b0baec
RootDevelop <robbert@rootdevelop.nl>
2021-11-30 06:25:02 -0800

    Update Helix.cs

--- Helix.cs
+++ Helix.cs
@@ -1,15 +1,22 @@
-﻿using System.Text;
+using System.Text;
 using System.Text.Json;
 
 public static class Helix<T> where T : new()
 {
     private static T _data = default!;
 
+    /// <summary>
+    /// Gets the current instance of T
+    /// </summary>
+    /// <returns></returns>
     public static T Get()
     {
         return _data;
     }
 
+    /// <summary>
+    /// Registers a new instance of Helix T and load existing data into memory
+    /// </summary>
     public static void Register()
     {
         var data = File.Exists(GetFileName()) ? File.ReadAllText(GetFileName(), Encoding.UTF8) : string.Empty;
@@ -17,6 +24,9 @@ public static class Helix<T> where T : new()
         _data = (data == string.Empty ? new T() : JsonSerializer.Deserialize<T>(data))!;
     }
 
+    /// <summary>
+    /// Persists the current instance of T to disk, use environment variable HELIX to configure a custom path
+    /// </summary>
     public static void Persist()
     {
         if (_data == null) return;
@@ -33,6 +43,6 @@ public static class Helix<T> where T : new()
             name = typeof(T).GenericTypeArguments.First().Name;
         }
 
-        return name.ToLowerInvariant().Trim() + ".helix";
+        return Path.Combine(Environment.GetEnvironmentVariable("HELIX") ?? string.Empty, name.ToLowerInvariant().Trim() + ".helix");
     }
 }
